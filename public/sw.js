f4658fb410fe7c8226da6ffa6eec95b34b26dfd6
Pete Lee <peterjslee@gmail.com>
2026-04-03 22:57:27 -0500

    Resolve merge conflicts from stash pop

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // InPlace Service Worker — v1.57.14
-const CACHE_NAME = 'inplace-build-2cbee21e-mnjs3tbl';
-const SW_VERSION = 'build-2cbee21e-mnjs3tbl';
+const CACHE_NAME = 'inplace-build-2cbee21e-mnjswve4';
+const SW_VERSION = 'build-2cbee21e-mnjswve4';
 const STATIC_ASSETS = [
   '/',
   '/css/styles.css',
